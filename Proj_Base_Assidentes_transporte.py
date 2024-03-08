b072a8e47d39ed000045653c8fb9479e39699be9
Marcos Gregorio Farias Lima <gregoriodata@gmail.com>
2024-03-08 15:56:58 -0300

    Cod modificado, foi retirada a estrutura de repeticao e condicional, aplicando a bilioteca bandas e substituindo as chamadas pelo metodo DataFrame.

--- Proj_Base_Assidentes_transporte.py
+++ Proj_Base_Assidentes_transporte.py
@@ -12,53 +12,78 @@ serie_id = 85 # Óbitos em acidentes de transporte
 abrangencia = 3 # Abrangência da Série. Ex: 1 - País | 2 - Região | 3 - UF | 4 - Município
 tema_id = 85
 
-#url_serie = f'https://www.ipea.gov.br/atlasviolencia/api/v1/serie/{tema_id}'
-
 url_valor = f'https://www.ipea.gov.br/atlasviolencia/api/v1/valores-series/{serie_id}/{abrangencia}'
 
-
-
 requisicao = rq.request("GET",url_valor)  #Requisição API metodo GET
-print(requisicao) #Teste Requisição API
 retor_requi = json.loads(requisicao.text) #Converter os dados em dicionario text - metodo 
 
-print(type(retor_requi))
-
-# df = pd.DataFrame(retor_requi) 
-# print(df)
-
-# df2 = df['sigla']
-# df2 = df2.drop_duplicates()
-# print(df2)
-# df.to_csv('tbl.csv')
-# df.to_parquet('df.parquet.gzip',compression='gzip')
+df = pd.DataFrame(retor_requi) #Transformar dic em Data Frame
 
 
-# print(len(retor_requi)) #Tamanho da lista
-# print(retor_requi[:1])
+# print(type(df))
+
+
+# print(df.shape) #Quantidade de linhas e colunas do DataFrame
+# print(df.index) #Descrição do Index
+# print(df.columns)#Colunas presentes no DataFrame
+# print(df.count()) #Contagem de dados não-nulos
+# print(df['cod'])#Realizando o print de uma coluna selecionada;
+# print(df[['cod', 'sigla']])#Realizando o print de duas coluna selecionada;
+# print(df.describe())  --- ??
+
+
 
 # dim_UF = []  #Dimensão localidade 
 # print(type(dim_UF))
-
 # for i in (retor_requi):
 #        if not (i['cod'], i['sigla']) in dim_UF:
 #         dim_UF.append((i['cod'], i['sigla']))
-
 # #print(dim_UF)
-
 # print("------------------------------")
 
-# tbl_fato = [] #Tabela de registros dos fatos ocorridos.
 
-# for i in (retor_requi):
-#     if not (i['cod'], i['valor'], i['periodo']) in tbl_fato:
-#         tbl_fato.append((i['cod'], i['valor'], i['periodo']))
+df_dim_Localidade = df[['cod', 'sigla']]
+df_dim_Localidade = df_dim_Localidade.drop_duplicates()
 
-# print(tbl_fato)
-    
+print(df_dim_Localidade.count())
+# print(df_dim_Localidade)
+
+print("------------------------------") #891 x 2c / cod 27 - sigla 27 
+
 # dim_date = [] # Tabela dim data
 
 # for i in (retor_requi):
 #      if not i['periodo'] in dim_date:
 #          dim_date.append(i['periodo'])
 # print(dim_date)
+
+df_dim_Date = df['periodo']
+df_dim_Date = df_dim_Date.drop_duplicates()
+
+print(df_dim_Date.count())
+
+print("------------------------------") #891 / 33
+
+# tbl_fato = [] #Tabela de registros dos fatos ocorridos.
+# for i in (retor_requi):
+#     if not (i['cod'], i['valor'], i['periodo']) in tbl_fato:
+#         tbl_fato.append((i['cod'], i['valor'], i['periodo']))
+# print(tbl_fato)
+
+df_tbl_Fato = df[['cod','valor','periodo']]
+print(df_tbl_Fato.count())
+
+print("------------------------------") #891
+
+
+# df2 = df['sigla']
+# df2 = df2.drop_duplicates()
+# print(df2)
+# df.to_csv('tbl.csv')
+# df.to_parquet('df.parquet.gzip',compression='gzip')
+# print(len(retor_requi)) #Tamanho da lista
+# print(retor_requi[:1])
+# 
+
+
+    
